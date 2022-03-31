822909b5c70ffa0f6c9e39abf2559acda09fa18f
nainia-youness <nainiayouness@gmail.com>
2022-03-31 10:28:54 +0000

    created bakend project

--- backend/edito_rh_backend/employe/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeConfig(AppConfig):
-    name = 'employe'
--- backend/edito_rh_backend/employe/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('employés/', views.home, name="employé_home"),
-]
